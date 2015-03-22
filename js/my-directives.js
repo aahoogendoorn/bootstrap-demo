f7aa8ac077a060c1e93802f835d4267e45560798
aahoogendoorn <aahoogendoorn@gmail.com>
2015-03-23 00:03:32 +0100

    More AngularJS directive examples

---
+++ js/my-directives.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Sander on 22-3-2015.
+ */
+
+var myDirectives = angular.module("myDirectives", []);
+
+myDirectives.directive('myButton', function(){
+    return {
+        restrict: 'AE',
+        templateUrl: 'directives/my-button.html',
+        replace: true,
+        transclude: true,
+        scope: { model: '=model' }
+    }
+});
+
+myDirectives.directive('myJumbo', function(){
+    return {
+        restrict: 'AE',
+        replace: true,
+        transclude: true,
+        templateUrl: 'directives/my-jumbo.html',
+        scope: { model: '=model' }
+    }
+});
+
+myDirectives.directive('myNavbar', function(){
+    return {
+        restrict: 'AE',
+        replace: true,
+        transclude: true,
+        templateUrl: 'directives/my-navbar.html'
+    }
+});
+
+myDirectives.directive('myModal', function(){
+    return {
+        restrict: 'AE',
+        replace: true,
+        transclude: true,
+        templateUrl: 'directives/my-modal.html'
+    }
+});
